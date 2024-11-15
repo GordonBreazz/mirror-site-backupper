61593413c3c8e7f64d31a0c1c8f128eb998d3851
Gordon <miavergo@outlook.com>
2024-11-15 16:22:41 +0800

    Редактирование настроек скрипта

--- backup_script.sh
+++ backup_script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-### Блок настроек скрипта
+### Блок настроек скрипта ###
 
 ## Параметры скрипта на локальтной машине
 
-LOCAL_DIR="/home/site-backup/timeweb-newvsgaki"               # Локальная папка для бекапа
+LOCAL_DIR_PATH="/home/site-backup"                            # Путь к локальной папки для бекапа
 
-NAME_BACKUP_DIR="site-mirror"                                 # Название папки зеркала 
+NAME_LOCAL_DIR="timeweb-newvsgaki"                            # Название локальной папки для Бекапа
+NAME_MIRROR_DIR="site-mirror"                                 # Название папки для зеркала удалённого сервера 
 NAME_DB_DIR=".db_dumps"                                       # Название папки для дампов БД (часть зеркала)
 
 NAME_LOGS_DIR="logs"                                          # Название папки для логов
@@ -15,41 +16,42 @@ NAME_DELTA_DIR="backup_delta"                                 # Название
 NAME_MAIN_LOG_FILE="backup.log"                               # Название лог-файла
 NAME_ERROR_LOG_FILE="backup_error.log"                        # Название лог-файла с ошибками
 NAME_RSYNC_LOG_FILE="rsync.log"                               # Название лог-файла с результатами работы RSYNC
-
-DB_CREDENTIALS_FILE="./db_credentials.conf"                   # Путь к файлу конфигурации c массивом с данными для подключения к базам данных
-UPDATED_FILES_LIST="$LOGS_DIR/update_files.txt"               # Путь для файла со списком обновлённых файлов
+NAME_CREDENTIALS_FILE="credentials.conf"                      # Название файла с конфигурации c массивом данных для подключения к базам данных
 
 ## Параметры скрипта закрытых для изменения 
 
-BACKUP_DIR="$LOCAL_DIR/$NAME_BACKUP_DIR"                      # Папка для бекапа файлов
+LOCAL_DIR="$LOCAL_DIR_PATH/$NAME_LOCAL_DIR"                   # Локальная папка для бекапа
+BACKUP_DIR="$LOCAL_DIR/$NAME_MIRROR_DIR"                      # Папка для зеркала удалённого сервера
 LOGS_DIR="$LOCAL_DIR/$NAME_LOGS_DIR"                          # Папка для логов
 DELTA_DIR="$LOCAL_DIR/$NAME_DELTA_DIR"                        # Папка для симлинков на обновлённые файлы
 
+UPDATED_FILES_LIST="$LOGS_DIR/update_files.txt"               # Путь для файла со списком обновлённых файлов
 MAIN_LOG_FILE="$LOGS_DIR/$NAME_MAIN_LOG_FILE"                 # Путь к файлу с логами
 ERROR_LOG_FILE="$LOGS_DIR/$NAME_ERROR_LOG_FILE"               # Путь к файлу с логами ошибок
 RSYNC_LOG_FILE="$LOGS_DIR/$NAME_RSYNC_LOG_FILE"               # Путь к файлу с логами работы RSYNC
 
 DATE=$(date +"%Y%m%d_%H%M%S")                                 # Дата и время для метки
-IS_FIRST=1                                                     # Переменная для определения первого запуска
+IS_FIRST=1                                                    # Переменная для определения первого запуска
 
 ## Подключение функций
 
-source ./credentials.conf                                     # Массив с данными для подключения к базам данных
+source "./$NAME_CREDENTIALS_FILE"                                  # Массив с данными для подключения к базам данных
 source ./functions/log.sh                                     # Функции логирования
 source ./functions/connection.sh                              # Функции проверки доступности удалённого сервера
 source ./functions/databases_dump_create.sh                   # Функции создания дампов баз данных
 source ./functions/rsync_files_copy.sh                        # Функции копирования файлов сайта
 source ./functions/backup_delta_symlinks.sh                   # Функции создания папки симлинков
-source ./functions/antivirus_check.sh                          # Функции проверки файлов антивирусом
+source ./functions/antivirus_check.sh                         # Функции проверки файлов антивирусом
 
-### Определение функций
+### Определение функций ###
 
-# Функция инициализации
+## Функция инициализации
 function init() {
     check_connection                                          # Проверка доступности удалённого сервера
+    mkdir -p "$LOCAL_DIR"                                     # Создаём директорию для бэкапов, если её нет
     #> "$RSYNC_LOG_FILE"                                      # Очищаем RSYNC log перед началом работы
     > "$MAIN_LOG_FILE"                                        # Очищаем log перед началом работы
-    mkdir -p "$BACKUP_DIR"                                    # Создаём директорию для бэкапов, если её нет    
+    mkdir -p "$BACKUP_DIR"                                    # Создаём директорию для зеркала удалённого сервера, если её нет    
     mkdir -p "$LOGS_DIR"                                      # Создаём директорию для логов, если её нет
 }
 
